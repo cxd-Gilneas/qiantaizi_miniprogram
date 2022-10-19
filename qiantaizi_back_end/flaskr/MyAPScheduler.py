e8a760e9a69e3b3c78b639d89d7ef5d6995cc2dc
ziao德 <cxd_Gilneas@163.com>
2022-10-20 00:27:38 +0800

    Update MyAPScheduler.py
    Update 1 file:
    1. MyAPScheduler.py : add a misfire_grace_time

--- qiantaizi_back_end/flaskr/MyAPScheduler.py
+++ qiantaizi_back_end/flaskr/MyAPScheduler.py
@@ -5,5 +5,5 @@ def my_job():
     requests.post('https://ziaode.ren:5000/admin')
 
 sched = BlockingScheduler()
-sched.add_job(my_job, 'cron', day_of_week = 'wed', hour = 8)  # 每周周三的8点自动进行初始化
+sched.add_job(my_job, 'cron', day_of_week = 'wed', hour = 8, misfire_grace_time = 3600)  # 每周周三的8点自动进行初始化，并给一个缓冲响应时间（这里设置了3600s，其实没必要，60s即可），避免因为响应过晚导致任务不执行
 sched.start()
